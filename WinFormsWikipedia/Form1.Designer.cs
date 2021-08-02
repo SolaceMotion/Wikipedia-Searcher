1d9056a3860ee7700a063f7c2d124bdf8601ef9a
SolaceMotion <blockte8@gmail.com>
2021-08-02 23:25:00 +0200

    Add files

---
+++ WinFormsWikipedia/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace WinFormsWikipedia
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Search = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Search
+            // 
+            this.Search.Location = new System.Drawing.Point(447, 188);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(72, 24);
+            this.Search.TabIndex = 1;
+            this.Search.Text = "Search\r\n";
+            this.Search.UseVisualStyleBackColor = true;
+            this.Search.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox1.Location = new System.Drawing.Point(104, 240);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(568, 200);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(258, 188);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(184, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(235, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(307, 46);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Wikipedia Searcher";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Search);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Search;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WinFormsWikipedia/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Net.Http;
+using HtmlAgilityPack;
+using HtmlDocument = HtmlAgilityPack.HtmlDocument;
+
+namespace WinFormsWikipedia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async Task<string> CallWikipedia(string text)
+        {
+            string responseBody = "";
+            try
+            {
+                string url = $"https://en.wikipedia.org/wiki/ {text}";
+                using HttpClient client = new HttpClient();
+                HttpResponseMessage response = await client.GetAsync(url);
+                responseBody = await response.Content.ReadAsStringAsync();
+                
+            } catch (Exception err)
+            {
+                Debug.WriteLine(err.Message);
+            }
+
+            return responseBody;
+
+        }
+
+        private async Task<long> GetDocument()
+        {
+            // Stopwatch object
+            Stopwatch watch = new Stopwatch();
+
+            if (textBox1.Text == "")
+            {
+                textBox1.PlaceholderText = "Search query cannot be empty.";
+                return 0;
+            }
+
+            watch.Start();
+
+            string document = await CallWikipedia(textBox1.Text);
+            watch.Stop();
+            HtmlDocument htmlDoc = new HtmlDocument();
+            
+            htmlDoc.LoadHtml(document);
+            var content = htmlDoc.GetElementbyId("bodyContent");
+            string input = content.InnerText;
+            input = input.Replace("\n", "").Replace("\t", "");
+            string output = "";
+            string[] parts = input.Split('\n');
+
+            foreach (var part in parts)
+            {
+                output += part.Trim();
+            }
+            richTextBox1.Text = output;
+
+            // Return API call execution time
+            return watch.ElapsedMilliseconds;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var timeElapsed = await GetDocument();
+            textBox1.Text = "";
+
+            if (timeElapsed != 0)
+            {
+                textBox1.PlaceholderText = $"Time elapsed: {timeElapsed}ms";
+            }
+            
+        }
+
+    }
+}
